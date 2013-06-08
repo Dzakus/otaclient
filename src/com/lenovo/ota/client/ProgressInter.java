8a6dec0b478632161036138cdc51d9e95839253c
otaserver <qvis.be@gmail.com>
2013-06-09 00:38:08 +0800

    first commit

---
+++ src/com/lenovo/ota/client/ProgressInter.java
@@ -0,0 +1,5 @@
+package com.lenovo.ota.client;
+
+public interface ProgressInter {
+	void progress(int progress,long completesize) ;
+}
---
+++ src/com/lenovo/ota/client/test.java
@@ -0,0 +1,5 @@
+package com.lenovo.ota.client;
+
+public class test {
+
+}
