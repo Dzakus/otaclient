efce1eeeb95076ca7967d6aac5b69e1c05126261
otaserver <qvis.be@gmail.com>
2013-06-09 13:55:26 +0800

    update package name

--- src/com/lenovo/ota/client/ProgressInter.java
+++
@@ -1,5 +0,0 @@
-package com.lenovo.ota.client;
-
-public interface ProgressInter {
-	void progress(int progress,long completesize) ;
-}
--- src/com/lenovo/ota/client/test.java
+++
@@ -1,5 +0,0 @@
-package com.lenovo.ota.client;
-
-public class test {
-
-}
